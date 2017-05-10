a42e22c88440dec4ef01f15cffd79f7da381ecd2
toddmoody <todd@moodsdigital.co.nz>
2017-05-10 16:12:16 +1200

    Initial MVP for review styling pending

---
+++ README.md
@@ -0,0 +1,24 @@
+--- Synopsis ---
+
+Single page website to promote a local Surfing event sponsored by Barrett Homes.
+
+The primary purpose of the website is to promote the event and congratulate participants from previous years (this will also increase the profile of the event in the local community and generate interest for the 2018 event).  
+
+Additionally, users can register their interest to enter the 2018 competition and by doing so they enter the draw to win a custom surfboard.  
+
+Note: This competition is not active, has not been approved by the client, and in draft format for testing/review purposes only.
+
+--- Integration ---
+
+Competitor images are pulled in via the 'flickr' API in JSON-P format.  
+
+--- Planned in next release ---
+
+- Styling across the website, in particular intro section, Hall Of Fame & Form
+- Additional image assets including graphic of surfboard to win
+- Mobile responsive updates
+- Sponsor logo footer to be populated.
+
+--- Credits ---
+
+- Barrett Homes for branding.
---
+++ assets/app.js
@@ -0,0 +1,36 @@
+// Document ready function to ensure that HTML loads prior to .js running
+$(document).ready(function() {
+  // click listener function set-up to listen for clicks across all "filter" buttons and toggle css based on mouse over and selected button
+  $(".hall-btn").click(function(){
+    $(".hall-btn").removeClass("selected");
+    $(this).addClass("selected");
+    // URL for public flickr feed including JSON call back string to convert XML into JSON
+    var flickerAPI = "https://www.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    //jQuery 'this' method to target button clicked.  Text captured using .text()
+    var filter = $(this).text(); 
+    // optional & manditory data used as criteria.  Later passed as an argument into the $.getJSON function below.
+    var flickerOptions = {
+      id: "137821035@N07",
+      tags: filter,
+      format: "json"
+    };
+
+    // HTML 'render function' using custom elements of the flickr JSON data
+    function displayPhotos(data) {
+      // create a local 'photoHTML' variable which is gradually built up into the required HTML structure incl. bootstrap responsive classes.
+      var photoHTML = '<div class="row">';
+      $.each(data.items, function (i, photo) {
+        photoHTML += '<div class="col-lg-3 col-sm-6">';
+        photoHTML += '<div class="thumbnail">';
+        photoHTML += '<img class="flickr-img" src="' + photo.media.m + '">';
+        photoHTML += '</div>';
+        photoHTML += '</div>';
+      });
+      photoHTML += '</div>';
+      // jQuery to traverse HTML and manipulation of the HTML using the photoHTML variable
+      $('#photos').html(photoHTML);
+    }
+    // get JSON function called including parameters for the URL, flickr data & callback function
+    $.getJSON(flickerAPI, flickerOptions, displayPhotos);
+  });
+}); // end ready
